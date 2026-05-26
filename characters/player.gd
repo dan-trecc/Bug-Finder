4f4a2f7e08eb7fd1c1b397a18dcb71341f7e26eb
dan-trecc <dan.trecc@gmail.com>
2026-05-26 15:07:39 -0400

    Update player.gd

--- characters/player.gd
+++ characters/player.gd
@@ -3,12 +3,12 @@ extends CharacterBody2D
 
 var movement_speed = 80.0
 
-func _physics_process(delta):
+func _physics_process(delta): #delta is 1 sec/framerate, keeps movement consistent at any framerate
 	movement()
 
-func movement(): # function that controls movement
-	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
-	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
-	var mov = Vector2(x_mov,y_mov)
-	velocity = mov.normalized() * movement_speed
-	move_and_slide() #causes character to move
+func movement(): #function that controls movement
+	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left") #If "right" is pressed, will move right, if "left" is pressed, will move left. either a 1 or 0
+	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up") #If "up" is pressed, will move up, if "down" is pressed, will move down. either a 1 or 0
+	var mov = Vector2(x_mov,y_mov) #points on the grid
+	velocity = mov.normalized() * movement_speed #velocity * mov with normalized included to make diagonal movement and linear movement the same
+	move_and_slide() #causes character to move, includes Delta in calculation
